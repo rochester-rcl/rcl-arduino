e1629c63ab4ca2f98bff8d88e19a9a2347d8a849
JoshR <jromphf@library.rochester.edu>
2015-07-28 12:36:30 -0400

    commented for instructional purposes

--- pre-college/lightTheremin/lightTheremin.ino
+++ pre-college/lightTheremin/lightTheremin.ino
@@ -1,22 +1,3 @@
-/*
-  Arduino Starter Kit example
- Project 6  - Light Theremin
- 
- This sketch is written to accompany Project 6 in the
- Arduino Starter Kit
- 
- Parts required:
- photoresistor
- 10 kilohm resistor 
- piezo
- 
- Created 13 September 2012
- by Scott Fitzgerald
- 
- http://arduino.cc/starterKit
- 
- This example code is part of the public domain 
-*/
 
 // variable to hold sensor value
 int sensorValue;
@@ -28,17 +9,25 @@ int sensorHigh = 0;
 const int ledPin = 13;
 
 void setup() {
+  
   // Make the LED pin an output and turn it on
   pinMode(ledPin, OUTPUT);
+  
   digitalWrite(ledPin, HIGH);
+  
   Serial.begin(9600);
+  Serial.println("Light Theremin Project!");
 
   // calibrate for the first five seconds after program runs
-  while (millis() < 5000) {
+  //Here we have a while loop, which will run while a condition is met (no variable, no check condition, no incrementation)
+  while (millis() < 5000) { //Here we're going to calibrate for 5 seconds
     // record the maximum sensor value
     sensorValue = analogRead(A3);
+    
     if (sensorValue > sensorHigh) {
+      
       sensorHigh = sensorValue;
+      
     }
     // record the minimum sensor value
     if (sensorValue < sensorLow) {
@@ -55,12 +44,17 @@ void loop() {
   Serial.println(sensorValue);
 
   // map the sensor values to a wide range of pitches
-  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 1000);
+  /*Here we're using the map function to map our values from our pin to a pitch value for our tone function. Map works like this: 
+   map(lowerBoundofCurrentRange, upperBoundofCurrentRange, lowerBoundOfTargetRange, upperBoundOfTargetRange)
+   */
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 1000); //so we map our sensor value to a new 'pitch' range from 50 - 1000
+
 
   // play the tone for 20 ms on pin 8
   tone(8, pitch, 20);
 
-  // wait for a moment
+  // wait for 10ms 
   delay(10);
+  
 }//Program in some options to turn it off
 
